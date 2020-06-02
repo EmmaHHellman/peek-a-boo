98198534a2da0dfded56a8c103006bf445d1f56e
Micheal Hansen <micheal.hansen@gmail.com>
2020-06-02 10:18:39 -0700

    add clickable for emu and both
    co-authored-by: Emma Hellman <ehh801@icloud.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,18 @@
 $(document).ready(function() {
-  $(".clickable").click(function() {
+  $(".clickable-walrus").click(function() {
     $("#walrus-showing").toggle();
     $("#walrus-hidden").toggle();
   });
+
+  $(".clickable-emu").click(function() {
+    $("#emu-showing").toggle();
+    $("#emu-hidden").toggle();
+  });
+
+  $(".show_all_images").click(function() {
+    $("#walrus-showing").toggle();
+    $("#walrus-hidden").toggle();
+    $("#emu-showing").toggle();
+    $("#emu-hidden").toggle();
+  })
 });
