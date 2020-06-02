d2853b72d3ccf55f8eb837f487489a1e477cadd4
Micheal Hansen <micheal.hansen@gmail.com>
2020-06-02 10:36:41 -0700

    add fade toggle for kitten and slide toggle for puppy
    co-authored-by: Emma Hellman <ehh801@icloud.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,29 @@ $(document).ready(function() {
   $(".clickable-walrus").click(function() {
     $("#walrus-showing").toggle();
     $("#walrus-hidden").toggle();
-  });
+  })
 
   $(".clickable-emu").click(function() {
     $("#emu-showing").toggle();
     $("#emu-hidden").toggle();
-  });
+  })
 
   $(".show_all_images").click(function() {
     $("#walrus-showing").toggle();
     $("#walrus-hidden").toggle();
     $("#emu-showing").toggle();
     $("#emu-hidden").toggle();
+    $("#both-showing").toggle();
+    $("#both-hidden").toggle();
+  })
+
+  $(".clickable-kitten").click(function() {
+    $("#kitten-showing").fadeToggle();
+    $("#kitten-hidden").fadeToggle();
+  })
+
+  $(".clickable-puppy").click(function() {
+    $("#puppy-showing").slideToggle();
+    $("#puppy-hidden").slideToggle();
   })
 });
